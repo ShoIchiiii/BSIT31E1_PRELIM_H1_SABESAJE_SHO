5905a35823b63171e01c70ea7df54cc38f0d1869
KevinRoque1 <kcndacc@gmail.com>
2026-06-20 10:16:22 +0800

    Create Class3.cs

---
+++ ConsoleApp1/ConsoleApp1/Class3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentManagementSystem
+{
+    public class Student
+    {
+        public string Name;
+        public List<int> Grades;
+
+        public Student(string name, List<int> grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+
+        public double Average()
+        {
+            return Grades.Count == 0 ? 0.0 : Grades.Average();
+        }
+    }
+}
