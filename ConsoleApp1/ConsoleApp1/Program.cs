ddcc7f5f473628b1a40e7b14dd6a5fa0d9ab8ff9
KevinRoque1 <kcndacc@gmail.com>
2026-06-20 09:57:13 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,2 @@
 ﻿
-public class Student
-{
-    public string Name;
-    public List<int> Grades;
-
-    public Student(string name, List<int> grades)
-    {
-        Name = name;
-        Grades = grades;
-    }
-
-    public double Average()
-    {
-        return Grades.Count == 0 ? 0.0 : Grades.Average();
-    }///text
-}
    
