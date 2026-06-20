ff841d38f9574055a1773e58757e4c265e7e3451
KevinRoque1 <kcndacc@gmail.com>
2026-06-20 09:55:05 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace StudentManagementSystem
+﻿
+public class Student
 {
-    public class Student
+    public string Name;
+    public List<int> Grades;
+
+    public Student(string name, List<int> grades)
     {
-        public string Name;
-        public List<int> Grades;
+        Name = name;
+        Grades = grades;
+    }
 
-        public Student(string name, List<int> grades)
-        {
-            Name = name;
-            Grades = grades;
-        }
-
-        public double Average()
-        {
-            return Grades.Count == 0 ? 0.0 : Grades.Average();
-        }
+    public double Average()
+    {
+        return Grades.Count == 0 ? 0.0 : Grades.Average();
     }
 }
+   
