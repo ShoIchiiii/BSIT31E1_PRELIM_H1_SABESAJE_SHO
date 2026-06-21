7d33e7f2c74a1b4f24e402aefd2321d6b6c3c768
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-06-21 10:58:46 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -40,7 +40,7 @@ class Program
 
                 case "5":
                     running = false;
-                    Console.WriteLine("Goodbye!");
+                    Console.WriteLine("Exiting program... \n Goodbye!");
                     break;
 
                 default:
